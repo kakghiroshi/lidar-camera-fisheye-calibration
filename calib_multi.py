a64a7bfce2e2eda64656e8dbe008e44658531a17
kakghiroshi <77578976+kakghiroshi@users.noreply.github.com>
2023-02-01 15:26:16 +0800

    Update calib_multi.py

--- calib_multi.py
+++ calib_multi.py
@@ -108,6 +108,13 @@ if __name__ == "__main__":
         # 主要功能是截取点云
         if cropped_flag==True:
             for a in range(num):
+                print("1) Press 'Y' twice to align geometry with negative direction of y-axis)
+                print("2) Press 'K' to lock screen and to switch to selection mode")
+                print("3) Drag for rectangle selection,")
+                print("   or use ctrl + left click for polygon selection")
+                print("4) Press 'C' to get a selected geometry")
+                print("5) Press 'S' to save the selected geometry")
+                print("6) Press 'F' to switch to freeview mode")
                 pcd = o3d.io.read_point_cloud(pcd_path)#if multi crop is chosen,save pointcloud as origin name  before the last cut out ,save it as crop1.pcd/crop2.pcd... at the last cut out 
                 o3d.visualization.draw_geometries_with_editing([pcd])
         #加载截取后的点云
